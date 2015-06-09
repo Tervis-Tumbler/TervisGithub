23dd483e0a1f8ed6cff2cfb3b722f61afff41db5
ChrisMagnuson <hourglasssand@gmail.com>
2015-06-09 14:43:47 -0400

    Initial commit

---
+++ TervisGithub.psm1
@@ -0,0 +1,32 @@
+﻿$GithubRootURL = "https://github.com/Tervis-Tumbler/"
+
+$TervisGithubPowerShellModules = @"
+TervisGithub
+TervisKanbanize
+New-MailToURI
+get-MultipleChoiceQuestionAnswered
+KanbanizePowerShell
+TrackITWebAPIPowerShell
+TrackITUnOfficial
+InvokeSQL
+"@ -split "`r`n"
+
+function import-AllTervisGithubPowerShellModules {
+    $PowerShellModulesPath = ($ENV:PSModulepath -split ";")[0]
+    cd $PowerShellModulesPath
+
+    $TervisGithubPowerShellModules | % {
+        git clone $($GithubRootURL + $_ + ".git")
+    }
+}
+
+function Update-PowerShellModulesFromGit {
+
+    $PowerShellModulesPath = ($ENV:PSModulepath -split ";")[0]
+    $DirectoriesWithModules = gci $PowerShellModulesPath
+    foreach ($Directory in $DirectoriesWithModules) {
+        cd $Directory.FullName
+        git pull
+    }
+    #(Get-Module -ListAvailable PowerShellIse*).path
+}
