16214d4c10f3ac566c60290882a75afb7e193f1c
ChrisMagnuson <hourglasssand@gmail.com>
2016-06-15 09:47:56 -0400

    Added initial script to bootstrap the rest of the process

---
+++ InitialBootstrap.ps1
@@ -0,0 +1,5 @@
+﻿$PowerShellModulesPath = ($ENV:PSModulepath -split ";")[0]
+cd $PowerShellModulesPath
+git clone https://github.com/Tervis-Tumbler/TervisGithub
+
+import-AllTervisGithubPowerShellModules
