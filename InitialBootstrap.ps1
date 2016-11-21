bf6e0dacc6cfe8902b27c64b429fc8ec543ec0fc
Chris Magnuson <hourglasssand@gmail.com>
2016-11-21 16:20:10 -0500

    fix new item

--- InitialBootstrap.ps1
+++ InitialBootstrap.ps1
@@ -1,5 +1,5 @@
 $PowerShellModulesPath = ($env:PSMODULEPATH -split {$_ -in ":",";"})[0]
-New-Item $PowerShellModulesPath -ErrorAction SilentlyContinue
+New-Item $PowerShellModulesPath -ErrorAction SilentlyContinue -Type Directory
 Set-Location $PowerShellModulesPath
 git clone https://github.com/Tervis-Tumbler/TervisGithub
 
