e426a002b2680efa9303cbb698104987de265956
Chris Magnuson <hourglasssand@gmail.com>
2018-07-22 11:00:01 -0400

    Remove line to set security protocol as it isn't needed

--- InitialBootstrap.ps1
+++ InitialBootstrap.ps1
@@ -6,7 +6,7 @@ $PowerShellModulesPath = if ($env:PSModulePath -match ";") {
 
 New-Item $PowerShellModulesPath -ErrorAction SilentlyContinue -Type Directory
 Set-Location $PowerShellModulesPath
-[System.Net.ServicePointManager]::SecurityProtocol += [System.Net.SecurityProtocolType]::Tls12
+
 git clone https://github.com/Tervis-Tumbler/TervisGithub
 git clone https://github.com/Tervis-Tumbler/WebServicesPowerShellProxyBuilder
 git clone https://github.com/Tervis-Tumbler/TervisMicrosoft.PowerShell.Utility
